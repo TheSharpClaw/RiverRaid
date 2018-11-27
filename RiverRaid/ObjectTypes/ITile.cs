1796bcf00d5f2315f336c47597e46f069136c0c7
TheSharpClaw <thesharpclaw@gmail.com>
2018-11-27 02:38:10 +0100

    Update ITile

--- RiverRaid/ObjectTypes/ITile.cs
+++ RiverRaid/ObjectTypes/ITile.cs
@@ -1,26 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-
-using Android.App;
-using Microsoft.Xna.Framework;
-using Microsoft.Xna.Framework.Graphics;
-
-using RiverRaid.ObjectTypes;
+﻿using RiverRaid.ObjectTypes;
 
 namespace RiverRaid.Objects.Tiles
 {
     interface ITile : IEntity
     {
-        Vector2 Position { get; set; }
-        Texture2D Texture { get; set; }
-        Rectangle BoundingBox { get; set; }
         bool ToDestroy { get; }
-
-        IComponent Stage { get; }
-
-        void Draw();
-        void Update();
     }
 }
